f4d1f521c92fccc0c6e7f71d18fb21722ec437b4
donyradjah <donyradjah@gmail.com>
2017-07-27 14:27:25 +0700

    - add vue js - migration contact

--- public/index.php
+++ public/index.php
@@ -42,7 +42,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 | Once we have the application, we can handle the incoming request
 | through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
+| the client's browser allowing them to enjoy the cre ative
 | and wonderful application we have prepared for them.
 |
 */
